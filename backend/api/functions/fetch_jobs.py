818c07afd1bfa5f95a0a434414d0cb55b0476b9b
Diego <diego.boost@gmail.com>
2025-04-15 20:01:32 +0200

    query string updated

--- backend/api/functions/fetch_jobs.py
+++ backend/api/functions/fetch_jobs.py
@@ -5,7 +5,7 @@ import requests
 def fetch_jobs_from_api(role: str, location: str):
     try:
         url = "https://jsearch.p.rapidapi.com/search"
-        querystring = {"query": f"{role} in {location}", "page": "1", "num_pages": "1"}
+        querystring = {"query": f"{role} in {location}", "page": "1", "num_pages": "1", "country": "it"}
         
         headers = {
             "X-RapidAPI-Key": os.getenv("RAPIDAPI_KEY"),
